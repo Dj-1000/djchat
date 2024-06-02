7fe7ff4a8348fe5ef717d260664d8b906b28204a
Dheeraj Sharma <djsharma1000@gmail.com>
2024-06-03 00:29:08 +0530

    removed __pychache__

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,7 +11,7 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
-    
+
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
@@ -22,6 +22,7 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         message = text_data_json["message"]
 
         # Send message to room group
+        print("Sending messages to room: ",message)
         await self.channel_layer.group_send(
             self.room_group_name, {
                 "type": "chat.message",
